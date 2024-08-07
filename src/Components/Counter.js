c56557fa777c0c80e0285c3a1a01096caf808c8e
Hassan Ali <hassanalibhatti648@gmail.com>
2024-08-07 13:14:00 +0500

    Fixes the document formating

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -5,15 +5,15 @@ import './Counter.css'
 export default function Counter() {
     const count = useSelector((state) => state.count)
     const dispatch = useDispatch()
-  return (
-    <div className='container'>
-        <div className="counter">
-            <h1 style={{marginBottom: 20}}>Counter: {count}</h1>
-            <button onClick={() => dispatch(increment())} className='buttons'>Increment</button>
-            <button onClick={() => dispatch(decrement())} className='buttons'>Decrement</button>
-            <button onClick={() => dispatch(incrementByTen())} className='buttons'>Increment By Ten</button>
-            <button onClick={() => dispatch(decrementByTen())} className='buttons'>Decrement By Ten</button>
-            </div>
+    return (
+        <div className='container'>
+                <div className="counter">
+                    <h1 style={{marginBottom: 20}}>Counter: {count}</h1>
+                    <button onClick={() => dispatch(increment())} className='buttons'>Increment</button>
+                    <button onClick={() => dispatch(decrement())} className='buttons'>Decrement</button>
+                    <button onClick={() => dispatch(incrementByTen())} className='buttons'>Increment By Ten</button>
+                    <button onClick={() => dispatch(decrementByTen())} className='buttons'>Decrement By Ten</button>
+                </div>
         </div>
-  )
+    )
 }
